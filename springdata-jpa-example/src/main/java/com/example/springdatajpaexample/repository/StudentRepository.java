6cd57dae2a9128a7db567a4b1a752970951e2237
unknown <hdilhara@gmail.com>
2020-01-03 23:54:07 +0530

    fsf fdsf

--- springdata-jpa-example/src/main/java/com/example/springdatajpaexample/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdatajpaexample.repository;
-
-import com.example.springdatajpaexample.modal.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
